2b7b075b2443a883586bd85a2578eb4024852582
DESKTOP-HSLH89D\USER <victiria4325@gmail.com>
2021-10-20 13:45:32 +0900

    10/20수정

---
+++ include.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+  $(document).ready(function()
+  {
+    $('#header').load('header.html')
+    /* id 지정을 통해서도 가능합니다. 
+         $("#header").load("header.html #navbar")
+         */
+  }
+  );
+</script>;
